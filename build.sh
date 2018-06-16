b3c14e7b4d26c627e79e86f97c6758bc484146c8
Sumit Maji <skmaji1@outlook.com>
2018-06-16 12:17:01 +0530

    Take image name from configuration file.

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@
 [[ "TRACE" ]] && set -x
 
 source config/config
-
-docker build --build-arg LDAP_DOMAIN=$DOMAIN_NAME --build-arg LDAP_HOSTNAME=$LDAP_HOSTNAME --build-arg BASE_DN=$DC  --build-arg LDAP_PASSWORD=sumit -t sumit/ldap:latest .
+source configuration
+docker build --build-arg LDAP_DOMAIN=$DOMAIN_NAME --build-arg LDAP_HOSTNAME=$LDAP_HOSTNAME --build-arg BASE_DN=$DC  --build-arg LDAP_PASSWORD=sumit -t $IMAGE_NAME .
